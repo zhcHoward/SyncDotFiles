c67be84b72d0ea9b0447738f51672221d844b568
Howard Zhu <zhc_howard@hotmail.com>
2016-09-02 10:02:26 +0800

    small fixs

--- sdf.py
+++ sdf.py
@@ -3,7 +3,8 @@
 
 """
 Usage:
-  sync_dotfiles.py [download | upload | push | pull]
+  sdf.py [push | pull]             Push cache to remote or update cache from remote.
+  sdf.py [download | upload]       Generate changes and then update local/repo files.
 
 Options:
   -h --help         Show this screen.
@@ -19,14 +20,17 @@ if __name__ == '__main__':
     args = docopt(__doc__, version='Sync Dot Files 0.1')
 
     sync = SyncDotfiles()
-    if args['download'] or not (args['download'] or args['upload']):
-        sync.download_change()
+    if args['download']:
+        # sync.download_change()
         print('download')
     elif args['upload']:
-        sync.upload_change()
+        # sync.upload_change()
         print('upload')
     elif args['pull']:
-        sync.pull()
+        # sync.pull()
         print('pull')
+    elif args['push']:
+        # sync.push()
+        print()
     else:
         print(__doc__)
