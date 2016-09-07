5559fbf6a3df72d2bdfbdb7e19438f9cb971e44d
Howard Zhu <zhc_howard@hotmail.com>
2016-09-07 14:30:01 +0800

    remove unecessary comments and add necessary ones

--- sdf.py
+++ sdf.py
@@ -3,12 +3,19 @@
 
 """
 Usage:
-  sdf.py [push | pull | download | upload | update_local | update_cache]
+    sdf.py [download | upload | push | pull | update_local | update_cache]
 
 Options:
-  -h --help         Show this screen.
-  -V --version      Show version.
-  -v --verbose      Show verbose info.(Not Implemented Yet!)
+    pull                git pull or clone, depends on cache files exists or not.
+    push                git add -> git commit -> git push
+    update_local        use cache files to update local dotfiles
+    update_cache        use local files to update cache dotfiles
+    download            first pull then update locals
+    upload              first update_cache then push
+
+    -h --help           Show this screen.
+    -V --version        Show version.
+    -v --verbose        Show verbose info.(Not Implemented Yet!)
 """
 
 from docopt import docopt
@@ -17,23 +24,23 @@ from sync_dotfiles import SyncDotfiles
 
 if __name__ == '__main__':
     args = docopt(__doc__, version='Sync Dot Files 0.1')
-    print(args)
+
     sync = SyncDotfiles()
     if args['download']:
-        sync.download_change()
         print('download')
+        sync.download_change()
     elif args['upload']:
-        # sync.upload_change()
         print('upload')
+        sync.upload_change()
     elif args['pull']:
-        sync.pull()
         print('pull')
+        sync.pull()
     elif args['push']:
-        # sync.push()
         print('push')
+        sync.push()
     elif args['update_local']:
-        sync.update_local()
         print('update_local')
+        sync.update_local()
     elif args['update_cache']:
         print('update_cache')
         sync.update_cache()
--- sync_dotfiles.py
+++ sync_dotfiles.py
@@ -10,14 +10,6 @@ import datetime
 
 
 class SyncDotfiles():
-    # TODO
-    # if platform.system() == 'Linux':
-    #     cache_home = os.path.join(local_home, '.mydotfiles')
-    # elif platform.system() == 'Windowns':
-    #     cache_home = 'd:\\my-configs'  # TODO change to \User folder
-    # else:
-    #     cache_home = os.path.dirname(os.path.abspath(__file__))
-
     def __init__(self):
         # init configs
         self.bakup_extension = '.bak'
@@ -42,6 +34,10 @@ class SyncDotfiles():
         self.update_cache()
         self.push()
 
+    def download_change(self):
+        self.pull()
+        self.update_local()
+
     def push(self):
         # git add->commit->push
         self.git.add('-A')
@@ -49,13 +45,11 @@ class SyncDotfiles():
         self.git.commit("-m '{}'".format(now))
         self.git.push()
 
-    def download_change(self):
-        self.pull()
-        self.update_local()
-
     def pull(self):
-        # create dir for remote files
-        # git pull
+        """git pull
+        If the cache_home does not exist, this function will clone
+        the git repository defined in your setting file.
+        """
         if not os.path.exists(self.cache_home):
             if not os.path.exists(self.cache_base):
                 os.makedirs(self.cache_base, exist_ok=True)
@@ -63,7 +57,45 @@ class SyncDotfiles():
         else:
             self.git.pull()
 
+    def update_cache(self):
+        """Using local dotfiles to update files in the cache.
+        If cache files exists, old files will be directly deleted.
+        No '.bak' files in local cache since all versions of files are stored on git.
+        """
+        for app, config in self.settings['configs'].items():
+            for path in config['paths']:
+                local_path, cache_path = self.get_paths(app, path)
+                self.check_src_and_dst(local_path, cache_path)
+                if os.path.isdir(local_path):
+                    shutil.rmtree(cache_path)
+                    shutil.copytree(local_path, cache_path)
+                else:
+                    shutil.copy2(local_path, cache_path)
+
+    def update_local(self):
+        """Using dotfiles in cache to update local dotfiles
+        If local files exists, old files will be renamed to 'name.bak' first.
+        If 'name.bak' exists, the old '.bak' file will be directly deleted
+        """
+        for app, config in self.settings['configs'].items():
+            for path in config['paths']:
+                local_path, cache_path = self.get_paths(app, path)
+                self.check_src_and_dst(cache_path, local_path)
+
+                try:
+                    if os.path.exists(local_path):
+                        os.rename(local_path, local_path + self.bakup_extension)
+                except OSError:
+                    shutil.rmtree(local_path + self.bakup_extension)
+                    os.rename(local_path, local_path + self.bakup_extension)
+
+                if os.path.isdir(cache_path):
+                    shutil.copytree(cache_path, local_path)
+                else:
+                    shutil.copy2(cache_path, local_path)
+
     def get_paths(self, app, path):
+        """Generate path of local and cache from path in configs."""
         path = os.path.normpath(path)
 
         if os.path.isabs(path):
@@ -90,51 +122,3 @@ class SyncDotfiles():
 
         if not os.path.exists(dst):
             os.makedirs(os.path.dirname(dst), exist_ok=True)
-
-    def update_cache(self):
-        for app, config in self.settings['configs'].items():
-            for path in config['paths']:
-                local_path, cache_path = self.get_paths(app, path)
-                self.check_src_and_dst(local_path, cache_path)
-                if os.path.isdir(local_path):
-                    shutil.rmtree(cache_path)
-                    shutil.copytree(local_path, cache_path)
-                else:
-                    shutil.copy2(local_path, cache_path)
-            #     shutil.copy2(local_path, cache_path)
-            # for conf_path in config['folders']:
-            #     local_path, cache_path = self.read_config(conf_path, app, True)
-            #     if os.path.exists(cache_path):
-            #         shutil.rmtree(cache_path)
-            #     shutil.copytree(local_path, cache_path)
-
-    def update_local(self):
-        for app, config in self.settings['configs'].items():
-            for path in config['paths']:
-                local_path, cache_path = self.get_paths(app, path)
-                self.check_src_and_dst(cache_path, local_path)
-
-                try:
-                    if os.path.exists(local_path):
-                        os.rename(local_path, local_path + self.bakup_extension)
-                except OSError:
-                    shutil.rmtree(local_path + self.bakup_extension)
-                    os.rename(local_path, local_path + self.bakup_extension)
-
-                if os.path.isdir(cache_path):
-                    shutil.copytree(cache_path, local_path)
-                else:
-                    shutil.copy2(cache_path, local_path)
-            #     if os.path.exists(target_path):
-            #         os.rename(target_path, target_path + self.bakup_extension)
-            #     if not os.path.exists(target_path):
-            #         os.makedirs(os.path.dirname(target_path), exist_ok=True)
-            #     shutil.copy2(source_path, target_path)
-            # for config_path in config['folders']:
-            #     config_path = os.path.normpath(config_path)
-            #     source_path = os.path.join(self.cache_home, app, config_path)
-            #     target_path = os.path.join(self.local_home, config_path)
-            #     if os.path.exists(target_path):
-            #         shutil.rmtree(target_path + self.bakup_extension)
-            #         os.rename(target_path, target_path + self.bakup_extension)
-            #     shutil.copytree(source_path, target_path)
